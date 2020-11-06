880d5f5d6c41f7c4614f158976362dc7bf0b5330
rt022748 <ramsey.tisher@cerner.com>
2020-11-06 08:25:24 -0500

    full screen

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#1d6cd2`,
         theme_color: `#1d6cd2`,
-        display: `standalone`,
+        display: `fullscreen`,
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
